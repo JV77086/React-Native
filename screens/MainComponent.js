6e25f0c136e1167bc01b106ed49940ec4fe81c57
Juan Valencia <juan77086@yahoo.com>
2023-07-15 01:33:13 +0700

    modified:   screens/MainComponent.js

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -3,12 +3,8 @@ import Constants from 'expo-constants';
 import CampsiteInfoScreen from './CampsiteInfoScreen';
 import DirectoryScreen from './DirectoryScreen';
 import { createStackNavigator } from '@react-navigation/stack';
-import {
-    createDrawerNavigator,
-    DrawerContentScrollView,
-    DrawerItemList
-} from '@react-navigation/drawer';
-import HomeScreen from './HomeScreen';
+import { createDrawerNavigator, DrawerContentScrollView, DrawerItemList} from '@react-navigation/drawer';
+import HomeScreen from './Homescreen';
 import AboutScreen from './AboutScreen';
 import ContactScreen from './ContactScreen';
 import { Icon } from 'react-native-elements';
